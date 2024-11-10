370fc3c04a02c8f1c31a28bb5b60e71d24af6728
VladimirSivac <morndes@gmail.com>
2024-11-10 20:34:55 +0200

    10 tasks completed
    Выполнен компьютерный практикум №7 (10 задач)

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 (a)
+            /*Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.ReadLine();*/
+            // Task 1 (b)
+            /*Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!");
+            Console.ReadLine();*/
+            // Task 2
+            /*Console.Write("Введите название футбольной команды: ");
+            string team_name = Console.ReadLine();
+            Console.WriteLine($"{team_name} - это чемпион!");
+            Console.ReadLine();*/
+            // Task 3
+            /*Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write($"{name} ");
+            Console.Write($"{surname}");
+            Console.ReadLine();*/
+            // Task 4
+            /*Console.Write("Название государства: ");
+            string country = Console.ReadLine();
+            Console.Write("Столица государства: ");
+            string capital = Console.ReadLine();
+            Console.Write($"Столица государства {country} - город {capital}");
+            Console.ReadLine();*/
+            // Task 5
+            /*Console.Write("Введите название романа: ");
+            string novel = Console.ReadLine();
+            Console.Write("Введите фамилию автора: ");
+            string writer = Console.ReadLine();
+            Console.Write($"Писатель {writer} - автор романа {novel}");
+            Console.ReadLine();*/
+            // Task 6
+            /*Console.Write("Введите название футбольного клуба: ");
+            string football_club = Console.ReadLine();
+            int length = football_club.Length;
+            Console.Write($"Количество символов футбольного клуба - {length}");
+            Console.ReadLine();*/
+            // Task 7
+            /*Console.WriteLine("Ты вчера был болен.");
+            Console.WriteLine("Измерь - ка температуру!");
+            Console.Write("Сообщи, какая у тебя температура: ");
+            double temperature = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ты здоров, дружок!");
+            Console.WriteLine("Можешь идти в школу.");
+            Console.WriteLine("Желаю успехов!");
+            Console.ReadLine();*/
+            // Task 8
+            /*Console.Write("Здравствуй, я компьютер, а тебя как зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Очень приятно, {name}.");
+            Console.Write("Сколько тебе лет? ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Ого! Целых {age} лет! Ты уже совсем взрослый!");
+            Console.ReadLine();*/
+            // Task 9
+            /*Console.Write("Как тебя зовут? ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!");
+            Console.Write("Сколько тебе лет? ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Какой у тебя любимый предмет в техникуме? ");
+            string subject = Console.ReadLine();
+            Console.Write("Какая у тебя любимая книга? ");
+            string book = Console.ReadLine();
+            Console.WriteLine($"Значит тебя зовут, {name}.");
+            Console.ReadLine();*/
+            // Task 10
+            /*Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{name}, ты родился в {2024 - age} году.");
+            Console.ReadLine();*/
+        }
+    }
+}
